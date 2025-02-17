ae33dadc8cdc7c636fff919aa26c264f4a019d4d
shiva-kukatla <leela.siva.kukatla@gmail.com>
2025-02-17 14:02:55 +0530

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,34 @@
+#prime number
+
+# number=int(input())
+# if number>1:
+#     for i in range(2,number):
+#         if number % i == 0:
+#             print(number,"is not a prime")
+#             break
+#     else:
+#         print(number,"is a prime number")
+# else:
+#     print(number , "is not a prime number")
+# o/p:
+# 4
+# 4 is not a prime  
+
+#sqrt and finding prime
+
+# import math 
+# num = int(input())
+# value=math.sqrt(num)
+# print("sqrt value:",value)
+# if value>1:
+#     for i in range(2,int(value)+1):
+#         if num % i == 0:
+#             print(value,"is not a prime")
+#             break
+#     else:
+#         print(value,"is a prime number")
+# else:
+#     print(value , "is not a prime number")
+# 9
+# sqrt value: 3.0
+# 3.0 is not a prime
